3ad8ddf0a9ea58c75aea68ef15802e410050eed9
Ganesh1617 <151633666+Ganesh1617@users.noreply.github.com>
2023-11-21 17:19:39 +0530

    dsw

--- SamapleProjectForJenkins/Program.cs
+++ SamapleProjectForJenkins/Program.cs
@@ -8,8 +8,17 @@ namespace SamapleProjectForJenkins
 {
     internal class Program
     {
+        public void Addition()
+        {
+            int a=10, b=20,c
+            c = a + b;
+            Console.WriteLine(c);
+        }
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Addition();
+            Console.ReadLine();
         }
     }
 }
